6c89a9f6987e0b069d8b07e27933d737c675d809
Creator125 <obedagualimpia1@gmail.com>
2022-04-14 17:21:49 -0500

    Se renombró una variable en 'devolución de dinero'

--- devolucion de dinero/devolucion de dinero.c
+++ devolucion de dinero/devolucion de dinero.c
@@ -3,26 +3,25 @@
 #include <stdio.h>
 
 int main(){
-  int valor_compra,valor_producto,devolucion;
+  int cantidad_dinero,valor_producto,devolucion;
   
   //Pidiemdo los datos para la compra
   printf("Ingrese su valor a pagar: $");
-  scanf("%d",&valor_compra); //Pidiendo el valor que ingresa el usuario
+  scanf("%d",&cantidad_dinero); //Pidiendo el valor que ingresa el usuario
   printf("\nIngrese el valor del producto: $");
   scanf("%d",&valor_producto); //Pidiendo el valor del producto
   
   //Codicional
-  if (valor_compra == valor_producto){
+  if (cantidad_dinero == valor_producto){
     //Si ambos valores son iguales imprimirá un mensaje
     printf("\nEl pago es exacto \n");
-  }else if(valor_compra < valor_producto){
+  }else if(cantidad_dinero < valor_producto){
     //Si el valor del usuario es menor al valor a pagar
     printf("\nDinero insuficiente \n");
   }else{
     //Se ejecutará si ningunas de las condiciones son verdaderas
-    
     //Opreacion para la devolucion
-    devolucion = valor_compra - valor_producto;
+    devolucion = cantidad_dinero - valor_producto;
     
     //Imprimiendo la devolucion
     printf("\nLa devolucion del dinero restantes es: $%d \n",devolucion);
