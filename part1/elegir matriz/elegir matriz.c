a2ff6ca909c26b3b32e7940a387fa01d917249ed
Creator125 <obedagualimpia1@gmail.com>
2022-06-03 09:20:14 -0500

    Se agruparon las 1ras practicas en part1

---
+++ part1/elegir matriz/elegir matriz.c
@@ -0,0 +1,13 @@
+//
+
+#include <stdio.h>
+
+int main(){
+  //Numeros en la matriz
+  int numeros[10] = {87,45,95,83,10,64,30,112,0,97};
+  int n;
+  
+  printf("Selecione un index del 0 al 9");
+  scanf("");
+  
+}
