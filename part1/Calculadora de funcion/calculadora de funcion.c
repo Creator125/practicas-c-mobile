e57d1bac2dc7a318af3d683ff658b03340f80913
Creator125 <obedagualimpia1@gmail.com>
2022-06-23 08:08:08 -0500

    De arregró c alculadora de funcion (aun sigue con errores)

--- part1/Calculadora de funcion/calculadora de funcion.c
+++ part1/Calculadora de funcion/calculadora de funcion.c
@@ -1,95 +1,60 @@
 #include <stdio.h>
 
-char operador,opcion; //Equivaldrá: +, -, *, /
-float resul;
-
-float main(){ //Funcion principal
-  float n1,n2;
+int main(){ //Funcion principal
+  int n1,n2;
+  char operador;
   
   printf("\tCalculadora\n"); //Presentacion
 
-  inicio:
   printf("Ingrese tu operacion:\n");
   /*En esta el usuario debe ingesar el numero,
   luego el operador y otro numero al final*/
   scanf("%f %c %f",&n1,&operador,&n2);
   
   if((operador == '+') || (operador == '-') || (operador == '*') || (operador == '/')){
-    calcular(n1,n2);
-    printf("%.f\n",resul);
+    calcular(n1,operador,n2);
   }else{
     printf("Error\n");
   }
   
-  conti_salida();
-  
   return 0;
 }
 
-float calcular(float a, float b){ //Esta funcion verificará el operador
-  if (operador == '+'){
-    resul = suma(a,b);
-  }else if (operador == '-'){
-    resul = resta(a,b);
-  }else if (operador == '*'){
-    resul = multiplica(a,b);
-  }else if (operador == '/'){
-    resul = dividir(a,b);
+void calcular(int a,char o, int b){ //Esta funcion verificará el operador
+  if (o == '+'){
+    suma(a,b);
+  }else if (o == '-'){
+    resta(a,b);
+  }else if (o == '*'){
+    multiplica(a,b);
+  }else if (o == '/'){
+    dividir(a,b);
   }
-
-  return resul;
 }
 
 
-float suma(float as, float bs){//Funcion sumar
-  float c;
+void suma(int as, int bs){//Funcion sumar
+  int c = as + bs;
   
-  c = as + bs;
-  return c;
+  printf("%i\n",c);
 }
 
 
-
-float resta(float ar, float br){//Funcion restar
-  float c;
+void resta(int ar, int br){//Funcion restar
+  int c = ar - br;
   
-  c = ar - br;
-  return c; 
+  printf("%i\n",c); 
 }
 
 
-
-float multiplica(float am, float bm){//Funcion multiplicar
-  float c;
+void multiplica(int am, int bm){//Funcion multiplicar
+  int c = am * bm;
   
-  c = am * bm;
-  return c; 
+  printf("%i\n",c); 
 }
 
-
-float dividir(float ad, float bd){//Funcion dividir
-  float c;
+void dividir(int ad, int bd){//Funcion dividir
+  int c = ad / bd;
   
-  c = ab / bd;
-  return c;
-}
-
-
-char conti_salida(){//La funcion de continuar osalida
-  do{
-    fflush(stdin);
-    
-    opciones:
-    printf("¿Deseas continuar?:\n (N)Salir, (S)continuar\n");
-    scanf("%c",&opcion);
-    
-    if((opcion != 'n') || (opcion != 'N')){
-      goto inicio;
-    }
-    
-    if((opcion != 's') || (opcion != 'S')){
-      goto opciones;
-    }
-    
-  }while((opcion != 'n') || (opcion != 'N'));
+  printf("%i\n",c);
 }
