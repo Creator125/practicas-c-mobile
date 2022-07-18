1f80630a91bb83743c1d57b55ac57740e68f11e4
Creator125 <obedagualimpia1@gmail.com>
2022-07-18 16:47:58 -0500

    Actualizacion en 'Matriz convertidora' (Ahora funciona sin errores)

--- part2/Matriz convertidora.c
+++ part2/Matriz convertidora.c
@@ -4,70 +4,69 @@
 
 void cargarNumero(int vectorentero[5]) //La funcion que cargará los elementos numericos
 {
+  printf("Ingrese 5 numeros: \n");
+  
   for (int i = 0; i < 5; i++){
-    printf("Ingrese un numero: ");
+    printf("> ");
     scanf("%i",&vectorentero[i]);
   }
 }
 
 void cargarCaracter(char vectorcaracter[5]) //La funcion que cargará los elementos caracteres
 {
+  printf("Ingrese 5 caracteres: \n");
+  
   for (int i = 0; i < 5; i++){
-    printf("Ingrese un caracter: ");
+    printf("> ");
     scanf(" %c",&vectorcaracter[i]);
   }
 }
 
-void convertirEntero(int vectorentero[5]) //Funcion para convertir los numeros del vector a caracter e imprimirlos
-{
-  char vectorcaracter[5];
-  
-  for (int i = 0; i < 5; i++){
-    vectorcaracter[i] =(int) vectorentero[i];
-    
-    printf("%c ",vectorentero[i]);
-  }
-}
-
-void convertirCaracter(char vectorcaracter[5]) //Funcion para convertir los caracteres del vector a numeros e imprimirlos
+void convertirEntero(char vectorcaracter[5]) //Funcion para convertir los numeros del vector a caracter e imprimirlos
 {
   int vectorentero[5];
   
   for (int i = 0; i < 5; i++){
-    vectorentero[i] =(char) vectorcaracter[i];
+    vectorentero[i] = (int) vectorcaracter[i];
     
-    printf("%i ",vectorcaracter[i]);
+    printf("%i ",vectorentero[i]);
   }
 }
 
-void opcion(int opcion){ //Opcion para elegir una opcion
+void convertirCaracter(int vectorentero[5]) //Funcion para convertir los caracteres del vector a numeros e imprimirlos
+{
+  int vectorcaracter[5];
+  
+  for (int i = 0; i < 5; i++){
+    vectorcaracter[i] = (char) vectorentero[i];
+    
+    printf("%c ",vectorcaracter[i]);
+  }
+}
+int main() //Funcion pricipal
+{
+  int opcion,numeros[5];
+  char caracteres[5];
+  
   printf("Elige una opcion: \n");
   printf("[1].Convertir de entero a caracter\n");
   printf("[2].Convertir de caracter a entero\n");
   scanf("%i",&opcion);
-}
-
-int main() //Funcion pricipal
-{
-  int n,numeros[5];
-  char caracteres[5];
   
-  opcion(n);
-  
-  if (n == 1){
+  if (opcion == 1){
     cargarNumero(numeros);
-    
-    printf("Tu Numeros son: \n");
-    
-    convertirEntero(caracteres);
-    
-    printf("\n");
-  }else if (n == 2){
-    cargarCaracter(caracteres);
     
     printf("Tu caracteres son: \n");
     
     convertirCaracter(numeros);
+    
+    printf("\n");
+  }else if (opcion == 2){
+    cargarCaracter(caracteres);
+    
+    printf("Tu numeros son: \n");
+    
+    convertirEntero(caracteres);
     
     printf("\n");
   }else{
